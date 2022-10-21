a56de37704e067dc5c8949a0fb8b14d2b217db81
Dr. K.D. Murray <foss@kdmurray.id.au>
2022-10-21 15:31:28 +0200

    Update location sheets

--- misc-utils/location-sheets/pdf-2xA5-to-A4.py
+++ misc-utils/location-sheets/pdf-2xA5-to-A4.py
@@ -11,13 +11,17 @@ def main():
     print ("2-up input " + sys.argv[1])
     input1 = PdfFileReader(open(sys.argv[1], "rb"))
     output = PdfFileWriter()
-    for iter in range (0, input1.getNumPages()-1, 2):
+
+    assert input1.getNumPages() % 2 == 0
+    half = int(input1.getNumPages() /  2)
+    for iter in range (0, half, 1):
         lhs = input1.getPage(iter)
-        rhs = input1.getPage(iter+1)
+        rhs = input1.getPage(iter+half)
         lhs.mergeTranslatedPage(rhs, lhs.mediaBox.getUpperRight_x(),0, True)
         output.addPage(lhs)
-        print (str(iter) + " "),
+        print(str(iter) + " ", end="")
         sys.stdout.flush()
+    print()
 
     print("writing " + sys.argv[2])
     outputStream = open(sys.argv[2], "wb")
